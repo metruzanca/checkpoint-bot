bcdce8e5a6a9a51cd82a5e3d3df50a5f8351e726
Sam Zanca <samuele@zanca.dev>
2025-11-26 14:23:06 -0500

    handlers

--- internal/server/bot.go
+++ internal/server/bot.go
@@ -1,17 +1,15 @@
 package server
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/metruzanca/checkpoint-bot/internal/database"
 	"github.com/metruzanca/checkpoint-bot/internal/server/commands"
-	"github.com/metruzanca/checkpoint-bot/internal/sqlc"
+	"github.com/metruzanca/checkpoint-bot/internal/server/handlers"
 	"github.com/spf13/viper"
 
 	"github.com/bwmarrin/discordgo"
@@ -33,16 +31,18 @@ func NewBot(token string, dbPath string) *Bot {
 	}
 }
 
-func (b *Bot) RegisterCommand(guildID string, cmd *discordgo.ApplicationCommand, callback func(s *discordgo.Session, i *discordgo.InteractionCreate)) error {
+func (b *Bot) registerCommand(guildID string, cmd *discordgo.ApplicationCommand) {
 	registeredCmd, err := b.DiscordClient.ApplicationCommandCreate(b.DiscordClient.State.User.ID, guildID, cmd)
 	if err != nil {
-		return fmt.Errorf("cannot create command %s in guild %s: %w", cmd.Name, guildID, err)
+		log.Error("cannot create command", "command", cmd.Name, "guild", guildID, "err", err)
+		return
 	}
-
-	b.DiscordClient.AddHandler(callback)
 	log.Debug("Registered command", "command", registeredCmd.Name, "guild", guildID)
+}
 
-	return nil
+func (b *Bot) registerHandlers() {
+	checkpointHandler := handlers.NewCheckpointHandler(b.DiscordClient, b.Database)
+	b.DiscordClient.AddHandler(checkpointHandler.CreateCheckpoint)
 }
 
 func (b *Bot) Start() error {
@@ -61,34 +61,8 @@ func (b *Bot) Start() error {
 	// Register commands for each guild
 
 	for _, guild := range b.DiscordClient.State.Guilds {
-		err := b.RegisterCommand(guild.ID, commands.CreateCheckpoint, func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			checkpoint, err := b.Database.CreateCheckpoint(context.Background(), sqlc.CreateCheckpointParams{
-				ScheduledAt: time.Now().Add(time.Hour * 24 * 14).Format(time.RFC3339),
-				ChannelID:   i.ChannelID,
-			})
-
-			if err != nil {
-				log.Error("cannot create checkpoint", "err", err)
-				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: "Error creating checkpoint",
-					},
-				})
-				return
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: fmt.Sprintf("Checkpoint created: %s", checkpoint.ID),
-				},
-			})
-		})
-		// If a command fails to register, log the error and continue
-		if err != nil {
-			log.Error("cannot register command", "command", commands.CreateCheckpoint.Name, "guild", guild.ID, "err", err)
-		}
+		b.registerCommand(guild.ID, commands.CreateCheckpoint)
+		b.registerHandlers()
 	}
 
 	// Send a message to the dev channel if it is set
@@ -107,23 +81,23 @@ func (b *Bot) Start() error {
 	return nil
 }
 
-func (b *Bot) Stop() {
-	// Unregister commands that we've registered from all guilds
-	// alternatively, delete all commands with:
-	// for _, guild := range b.Session.State.Guilds {
-	// 	registeredCommands, err := b.Session.ApplicationCommands(b.Session.State.User.ID, guild.ID)
-	// 	if err != nil {
-	// 		log.Error("Failed to get registered commands", "guild", guild.ID, "err", err)
-	// 		continue
-	// 	}
-	// 	for name, cmd := range registeredCommands {
-	// 		err := b.Session.ApplicationCommandDelete(b.Session.State.User.ID, guild.ID, cmd.ID)
-	// 		if err != nil {
-	// 			log.Error("Failed to delete command", "command", name, "guild", guild.ID, "err", err)
-	// 		}
-	// 	}
-	// }
+func (b *Bot) unregisterCommands() {
+	for _, guild := range b.DiscordClient.State.Guilds {
+		registeredCommands, err := b.DiscordClient.ApplicationCommands(b.DiscordClient.State.User.ID, guild.ID)
+		if err != nil {
+			log.Error("Failed to get registered commands", "guild", guild.ID, "err", err)
+			continue
+		}
+		for name, cmd := range registeredCommands {
+			err := b.DiscordClient.ApplicationCommandDelete(b.DiscordClient.State.User.ID, guild.ID, cmd.ID)
+			if err != nil {
+				log.Error("Failed to delete command", "command", name, "guild", guild.ID, "err", err)
+			}
+		}
+	}
+}
 
+func (b *Bot) Stop() {
 	b.DiscordClient.Close()
 
 	log.Info("Bot stopped gracefully.")
---
+++ internal/server/handlers/checkpoint.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/charmbracelet/log"
+	"github.com/metruzanca/checkpoint-bot/internal/database"
+	"github.com/metruzanca/checkpoint-bot/internal/sqlc"
+)
+
+type CheckpointHandler struct {
+	DiscordClient *discordgo.Session
+	Database      database.CheckpointDatabase
+}
+
+func NewCheckpointHandler(discordClient *discordgo.Session, database database.CheckpointDatabase) *CheckpointHandler {
+	return &CheckpointHandler{
+		Database:      database,
+		DiscordClient: discordClient,
+	}
+}
+
+func (h *CheckpointHandler) CreateCheckpoint(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	checkpoint, err := h.Database.CreateCheckpoint(context.Background(), sqlc.CreateCheckpointParams{
+		ScheduledAt: time.Now().Add(time.Hour * 24 * 14).Format(time.RFC3339),
+		ChannelID:   i.ChannelID,
+	})
+
+	if err != nil {
+		log.Error("cannot create checkpoint", "err", err, "channel", i.ChannelID)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Error creating checkpoint",
+			},
+		})
+		return
+	}
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("Checkpoint created: %s", checkpoint.ID),
+		},
+	})
+}
